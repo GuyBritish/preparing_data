fecc08ef6e461842635143ff5ee6186103599a59
Max <80323354+GuyBritish@users.noreply.github.com>
2021-06-06 18:57:51 +0700

    modify arguments

--- dataset2bin.py
+++ dataset2bin.py
@@ -11,17 +11,20 @@ import lfw
 
 parser = argparse.ArgumentParser(description='Package LFW images')
 # general
-parser.add_argument('--data-dir', default='', help='')
+parser.add_argument('--pairs-dir', default='', help='')
 parser.add_argument('--image-size', type=str, default='112,112', help='')
 parser.add_argument('--output', default='', help='path to save.')
+parser.add_argument('--data-dir', default='', help='path to data')
 args = parser.parse_args()
 lfw_dir = args.data_dir
+lfwpairs_dir = args.pairs_dir
 image_size = [int(x) for x in args.image_size.split(',')]
-lfw_pairs = lfw.read_pairs(os.path.join(lfw_dir, 'pairs.txt'))
-lfw_paths, issame_list = lfw.get_paths(lfw_dir, lfw_pairs, 'jpg') # or jpg
+lfw_pairs = lfw.read_pairs(os.path.join(lfwpairs_dir, 'pairs.txt'))
+lfw_paths, issame_list = lfw.get_paths(lfw_dir, lfw_pairs) # or jpg
 lfw_bins = []
 #lfw_data = nd.empty((len(lfw_paths), 3, image_size[0], image_size[1]))
 i = 0
+
 for path in lfw_paths:
   with open(path, 'rb') as fin:
     _bin = fin.read()
